e06bd6f74af21d83ef372191133f6f22cef2e118
furkan <furkanmumcu2@gmail.com>
2017-01-29 00:23:12 +0200

    figured out how to delete contextmenu item

--- app/src/main/java/com/example/furkanmumcu/wawrecorder/SavedRecordingsActivity.java
+++ app/src/main/java/com/example/furkanmumcu/wawrecorder/SavedRecordingsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,10 +13,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SavedRecordingsActivity extends AppCompatActivity {
 
-    ArrayAdapter adapter;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,35 +39,42 @@ public class SavedRecordingsActivity extends AppCompatActivity {
         String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
                 "WebOS","Ubuntu","Windows7","Max OS X"}; // example
 
-        adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+        ArrayList<String> lstt = new ArrayList<String>(Arrays.asList(mobileArray));
+
+
+        adapter = new ArrayAdapter<String>(this, R.layout.activity_listview,lstt );
 
         ListView listView = (ListView) findViewById(R.id.recordinglist);
         listView.setAdapter(adapter);
         registerForContextMenu(listView);
 
-    }
+    }/*ContextMenu menu*/
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    public void onCreateContextMenu(android.view.ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         if (v.getId() == R.id.recordinglist) {
-            ListView lv = (ListView) v;
-            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            //ListView lv = (ListView) v;
+            //AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) menuInfo;
 
-            menu.add(Menu.NONE,1,1,"one");
-            menu.add(Menu.NONE,2,2,"two");
+            //menu.add(Menu.NONE,1,1,"one");
+            //menu.add(Menu.NONE,2,2,"two");
 
+            super.onCreateContextMenu(menu, v, menuInfo);
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.contextmenu, menu);
         }
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case 1:
+            case R.id.deleteItem:
                 AppLog.logString("one basildi");
+                //AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
                 AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-                //AppLog.logString("removing item pos=" + info.position);
-                //adapter.remove(info.position);
+                AppLog.logString("removing item pos= " + info.position);
+                adapter.remove(adapter.getItem(info.position));
                 return true;
-            case 2:
+            case R.id.editItem:
                 AppLog.logString("two basildi");
             default:
                 return super.onContextItemSelected(item);
